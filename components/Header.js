8028b040aa157702f03940785f75c01472ec80b5
Ana Vi <dismal.damsel@gmail.com>
2024-09-14 18:46:18 +0300

    changed logo with lemon

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="flex justify-center mb-4">
+        <img
+          src="/images/logo.png"
+          alt={`${name} logo`}
+          className="h-16 w-auto rounded-full"
+        />
+      </div>
       <p className="text-2xl text-center dark:text-white">
-        <Link href="/">
-          {name}
-        </Link>
+        <Link href="/">{name}</Link>
       </p>
     </header>
   );
